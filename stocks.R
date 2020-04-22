bd2d775832850ed3c9d4a70748e66aa4d2cb7987
Ashwin Haritsa <ashwin.haritsa@gmail.com>
2020-04-22 12:33:57 -0400

    updates

--- stocks.R
+++ stocks.R
@@ -2,6 +2,7 @@ library(tidyquant)
 library(dplyr)
 library(ggplot2)
 library(xts)
+library(cpm)
 
 
 for (stock in c('AAPL','MSFT','SBUX','GOOG')){
@@ -13,4 +14,10 @@ for (stock in c('AAPL','MSFT','SBUX','GOOG')){
 #   geom_line(data = AAPL, aes(index(AAPL),AAPL.Adjusted,col='red')) +
 #   geom_line(data = SBUX, aes(index(SBUX),SBUX.Adjusted,col='green'))
 
+df <- data.frame(date=index(AAPL),coredata(AAPL))
+a <- processStream(df$AAPL.Adjusted,cpmType = 'Kolmogorov-Smirnov')
 
+# get detection points
+df %>% 
+  filter(row_number() %in% a[["detectionTimes"]]) %>%
+  View()
